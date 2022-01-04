5b9f2bf195b6b27fce411cac2f9bbed93d09eb69
myungsworld <myungsworld@gmail.com>
2022-01-04 15:09:22 +0900

    add tutorial

--- main.go
+++ main.go
@@ -2,46 +2,31 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"github.com/ethereum/go-ethereum/common"
+	"ethereum/config"
+	"ethereum/tutorial"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
-	"math"
-	"math/big"
 )
 
-var infuraURL = "https://mainnet.infura.io/v3/e983371ca418429cb62af7f7f7767830"
-
-// sudo npm install -g ganache-cli
-// ganache-cli ( 테스트 로컬 메인넷 사용 )
-var ganacheURL = "http://localhost:8545"
 func main() {
 
-	client , err := ethclient.DialContext(context.Background(), infuraURL)
+	// config.TestNet = 테스트넷 , config.EtherMainNet = 이더리움 메인 네트워크
+	client, err := ethclient.DialContext(context.Background(), config.TestNet)
 	if err != nil {
-		log.Fatalf("클라 접속 실패:%v", err)
+		log.Fatalf("이더리움 메인넷 접속 실패:%v", err)
 	}
 	defer client.Close()
 
-	ctx := context.Background()
+	// 마지막 블록 가져오기
+	//tutorial.GetLatestBlockNumber(client)
 
-	// nil 이면 마지막 블록을 가져옴
-	block, err := client.BlockByNumber(ctx, nil)
-	if err != nil {
-		log.Fatalf("블럭 가져오기 실패:%v",err)
-	}
+	// 해시값으로 블록 가져오기
+	//tutorial.GetSpecificBlockHash(client)
 
-	fmt.Println("이더리움 메인넷 마지막 블록 넘버:",block.Number())
+	// 지갑주소로 balance 가져오기
+	//addr := "0xca02616f14cb40bbc429886807967d84a07c256a"
+	//tutorial.GetEtherAmountFromAddress(client, addr)
 
-	addr := "0xca02616f14cb40bbc429886807967d84a07c256a"
-	address := common.HexToAddress(addr)
-
-	//웨이(wei)는 이더리움에서 사용하는 암호화폐인 이더의 가장 작은 단위이다. 1 웨이(wei)는 10-18 이더(ether)와 같다. 다시 말하면, 1 이더(ether)는 1018 웨이(wei)와 같다
-	// 1 이더리움 = 10^18 wei
-	balanceWei , err := client.BalanceAt(ctx,address,nil)
-	fmt.Println("웨이 단위 (10의 -18승):",balanceWei)
-	fBlance := new(big.Float)
-	fBlance.SetString(balanceWei.String())
-	balanceEther := new(big.Float).Quo(fBlance, big.NewFloat(math.Pow10(18)))
-	fmt.Println("이더 단위 :", balanceEther)
+	// 이더 지갑 생성
+	tutorial.GenerateWallet()
 }
---
+++ tutorial/1.lastest_block_number.go
@@ -0,0 +1,36 @@
+package tutorial
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+	"math/big"
+)
+
+var ctx = context.Background()
+
+func GetLatestBlockNumber(client *ethclient.Client) *big.Int {
+
+	// number 를 지정할수 있는데 nil 이면 마지막 블록 넘버를 가져옴
+	block, err := client.BlockByNumber(ctx, nil)
+	if err != nil {
+		log.Fatalf("블럭 가져오기 실패:%v",err)
+	}
+
+	fmt.Println("이더리움 메인넷 마지막 블록 넘버:",block.Number())
+
+	return block.Number()
+
+}
+
+func GetSpecificBlockHash(client *ethclient.Client) {
+	block , err := client.BlockByHash(ctx , common.HexToHash("0x6b1a5cdc5e51172c2f4eb97e3fea0b99b4cac1372fba99f7e4d3b75bc32f0472"))
+	if err != nil {
+		fmt.Println("client.BlockByHash Err :", err )
+		panic(err)
+	}
+
+	fmt.Println(block)
+}
