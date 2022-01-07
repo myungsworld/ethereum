452bcca45bef2d047209334621e74dea6b66b0bf
myungsworld <myungsworld@gmail.com>
2022-01-07 11:49:32 +0900

    set remix ide

---
+++ solidity/project/contracts/todo.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Todo{
+    address public owner;
+
+    Task[] tasks;
+
+    struct Task{
+        string content;
+        bool status;
+    }
+
+    constructor(){
+        owner = msg.sender;
+
+    }
+
+    modifier isOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function add(string memory _content) public isOwner {
+
+        tasks.push(Task(_content,true));
+    }
+
+    function get(uint _id) public view returns (Task memory) {
+       
+        return tasks[_id];
+    }
+
+    function list() public view returns (Task[] memory) {
+         
+        return tasks;
+    }
+
+    function update(uint _id, string memory _content) public { 
+
+        tasks[_id].content = _content;
+    }
+
+    function remove(uint _id) public {
+
+        for(uint i = _id; i < tasks.length - 1 ; i++) {
+            tasks[i] = tasks[i+1];
+        }
+        tasks.pop();
+    }
+}
---
+++ solidity/scripts/run.sh
@@ -0,0 +1,4 @@
+cd project
+
+remixd -s ~/go/src/ethereum/solidity/project/ --remix-ide https://remix.ethereum.org
+
